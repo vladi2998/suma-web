3458b8e64d18690c455816cc79bc8d3329cf0058
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-01-22 22:03:47 -0400

    chore: connect page

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -3,7 +3,7 @@ import AddingSection from '@/components/home/addingSection';
 export default function ConnectPage() {
 	return (
 		<div className="w-screen px-4">
-			<AddingSection showActionButton={false}/>
+			<AddingSection showActionButton={false} />
 		</div>
 	);
 }
