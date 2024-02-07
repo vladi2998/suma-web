ef32ab25e52c8183fb05aab3258d8ad20093ea48
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-02-06 22:13:51 -0400

    chore: title for myRouteSection in mobile screen

--- src/components/home/myRouteSection.tsx
+++ src/components/home/myRouteSection.tsx
@@ -1,16 +1,13 @@
 'use client';
 
 import Image from 'next/image';
-import H1 from '../H1';
 import backgroundGirls from '../../../public/PNG/background-girls.png';
-import ForwardButton from '../buttons/forwardButton';
-import Link from 'next/link';
 import SearchBar from '../searchbar';
 
 export default function MyRouteSection() {
 	return (
 		<div className="w-full h-72 md:h-96 lg:h-132 xl:h-screen md:flex items-start justify-center relative mt-2">
-			<div className="absolute top-8 w-auto md:top-24 left-4 lg:left-24 md:w-96 lg:w-160 z-10">
+			<div className="absolute top-8 w-1/2 md:top-24 left-2 md:left-4 lg:left-24 md:w-96 lg:w-160 z-10">
 				<h1 className="w-full text-xl md:text-4xl lg:text-8xl font-bold text-start">
 					Plataforma de Networking
 				</h1>
