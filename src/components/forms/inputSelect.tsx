2740a47dccc2901ed7292efb4d7ea78c28c5cef6
Daniel Vieira <danielviei@hotmail.com>
2024-02-06 22:23:49 -0400

    feat: update register to let users register as undergraduate, postgraduate or teacher

--- src/components/forms/inputSelect.tsx
+++ src/components/forms/inputSelect.tsx
@@ -1,41 +1,38 @@
-import { Path, RegisterOptions, UseFormRegister } from 'react-hook-form';
-
-type ItemValue = {
-	value: string | number;
-	label: string;
+export type ItemValue = {
+  value: string | number;
+  label: string;
 };
 
-type InputProps = {
-	label: Path<any>;
-	register: UseFormRegister<any>;
-	required?: boolean;
-	placeholder?: string;
-	validation?: RegisterOptions<any, string> | undefined;
-	errors: any;
-	values: ItemValue[];
-};
+interface InputProps {
+  label: string;
+  register: any;
+  placeholder?: string;
+  validation?: any;
+  errors?: any;
+  values: ItemValue[];
+  onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void; // Agrega esta línea
+}
 
 export default function SelectField({
-	label,
-	register,
-	placeholder,
-	validation,
-	errors,
-	values,
+  label,
+  register,
+  placeholder,
+  validation,
+  errors,
+  values,
+  onChange,
 }: InputProps) {
-	return (
-		<>
-			<select
-				className="text-sm md:text-base hover:cursor-pointer hover:scale-105 transform transition duration-300 w-full h-auto bg-light-gray flex flex-row items-center justify-between px-4 py-6 rounded-l-full rounded-r-full appearance-none"
-				{...register(label, validation)}>
-				{values.map((item, idx) => (
-					<option
-						value={item.value}
-						key={idx}>
-						{item.label}
-					</option>
-				))}
-			</select>
-		</>
-	);
+  return (
+    <select
+      className="text-sm md:text-base hover:cursor-pointer hover:scale-105 transform transition duration-300 w-full h-auto bg-light-gray flex flex-row items-center justify-between px-4 py-6 rounded-l-full rounded-r-full appearance-none"
+      {...register(label, validation)}
+      onChange={onChange}
+    >
+      {values.map((item, idx) => (
+        <option value={item.value} key={idx}>
+          {item.label}
+        </option>
+      ))}
+    </select>
+  );
 }
