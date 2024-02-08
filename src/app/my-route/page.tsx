71f14a592a1056d878a4e8ad3921b69f7311b762
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-02-07 21:43:06 -0400

    feat: new my route page

--- src/app/my-route/page.tsx
+++ src/app/my-route/page.tsx
@@ -1,3 +1,15 @@
+import BrandingSection from '@/components/my-route/brandingSection';
+import ProfileSection from '@/components/my-route/profileSection';
+
 export default function MyRoutePage() {
-	return <p>Mi ruta</p>;
+	return (
+		<>
+			<div className="w-full px-4 my-4">
+				<ProfileSection />
+			</div>
+			<div className="w-full px-12 my-4">
+				<BrandingSection />
+			</div>
+		</>
+	);
 }
