ff91d8cf87cda009c6c85c781ee712aa04980731
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-02-24 22:57:58 -0400

    feat: new no-scroll utillity to hide the scroll

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -95,7 +95,22 @@ const config = {
 			},
 		},
 	},
-	plugins: [require('tailwindcss-animate')],
+	plugins: [
+		require('tailwindcss-animate'),
+		function ({ addUtilities }: any) {
+			const newUtilities = {
+				'.no-scrollbar::-webkit-scrollbar': {
+					display: 'none',
+				},
+				'.no-scrollbar': {
+					'-ms-overflow-style': 'none',
+					'scrollbar-width': 'none',
+				},
+			};
+
+			addUtilities(newUtilities);
+		},
+	],
 } satisfies Config;
 
 export default config;
