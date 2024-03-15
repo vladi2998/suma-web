3deec7ebc21d92c64f827a2f7abd693553c81f11
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-03-14 22:19:16 -0400

    mend

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
 import { z } from 'zod';
+import bcrypt from 'bcrypt';
+import { getUser } from '@/utils/auth';
 
 export const { auth, signIn, signOut } = NextAuth({
 	...authConfig,
@@ -11,6 +13,17 @@ export const { auth, signIn, signOut } = NextAuth({
 				const parsedCredentials = z
 					.object({ email: z.string().email(), password: z.string().min(6) })
 					.safeParse(credentials);
+
+				if (parsedCredentials.success) {
+					const { email, password } = parsedCredentials.data;
+					const user = await getUser(email);
+					if (!user) return null;
+					const passwordsMatch = await bcrypt.compare(password, user.password);
+					if (passwordsMatch) return user;
+				}
+
+				console.log('Invalid credentials');
+				return null;
 			},
 		}),
 	],
