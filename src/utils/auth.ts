0c8e5c70a953e09fbb0d57ba5227b6923a465d04
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-03-14 22:13:49 -0400

    mend

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,44 +1,49 @@
-import axiosConfigInstance from "@/config/axiosCofig";
-import { log } from "console";
+import axiosConfigInstance from '@/config/axiosCofig';
+import { log } from 'console';
 
 export const getUser = async (data: any) => {
-  try {
-    const request = await axiosConfigInstance.post("/token/", data);
-    return request;
-  } catch (err) {
-    console.log("error: ", err);
-  }
+	try {
+		const request = await axiosConfigInstance.post('/token/', data);
+		if (request.status === 200) {
+			return request.data;
+		} else {
+			return null;
+		}
+	} catch (err) {
+		console.log('error: ', err);
+		return null;
+	}
 };
 
 export const registerUndergraduateStudent = async (data: any) => {
-  try {
-    const request = await axiosConfigInstance.post(
-      "/register/undergraduate_student/",
-      data
-    );
-    return request;
-  } catch (err) {
-    console.log("error: ", err);
-  }
+	try {
+		const request = await axiosConfigInstance.post(
+			'/register/undergraduate_student/',
+			data
+		);
+		return request;
+	} catch (err) {
+		console.log('error: ', err);
+	}
 };
 
 export const registerPostGraduateStudent = async (data: any) => {
-  try {
-    const request = await axiosConfigInstance.post(
-      "/register/postgraduate_student/",
-      data
-    );
-    return request;
-  } catch (err) {
-    console.log("error: ", err);
-  }
+	try {
+		const request = await axiosConfigInstance.post(
+			'/register/postgraduate_student/',
+			data
+		);
+		return request;
+	} catch (err) {
+		console.log('error: ', err);
+	}
 };
 
 export const registerTeacher = async (data: any) => {
-  try {
-    const request = await axiosConfigInstance.post("/register/teacher/", data);
-    return request;
-  } catch (err) {
-    console.log("error: ", err);
-  }
+	try {
+		const request = await axiosConfigInstance.post('/register/teacher/', data);
+		return request;
+	} catch (err) {
+		console.log('error: ', err);
+	}
 };
