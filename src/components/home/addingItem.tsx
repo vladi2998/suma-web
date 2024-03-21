b63191d97b7260bd04a8122411e68d307ca64f31
Daniel Vieira <danielviei@hotmail.com>
2024-03-21 19:50:47 -0400

    feat: connect sumemos with backend

--- src/components/home/addingItem.tsx
+++ src/components/home/addingItem.tsx
@@ -6,16 +6,16 @@ import { UserProfileModal } from '../connect/userProfileModal';
 type AddingProps = {
 	name: string;
 	career: string;
-	master?: string;
-	specialist?: string;
+	study_level?: string;
+	charge?: string;
 	href: string;
 	img: string | StaticImport;
 };
 export default function AddingItem({
 	name,
 	career,
-	master,
-	specialist,
+	study_level,
+	charge,
 	href,
 	img
 }: AddingProps) {
@@ -31,8 +31,8 @@ export default function AddingItem({
 			<div className="w-full h-5/12 flex flex-col items-start text-start">
 				<h1 className="text-2xl font-bold">{name}</h1>
 				<p>{career}</p>
-				<p>{master}</p>
-				<p>{specialist}</p>
+				<p>{study_level}</p>
+				<p>{charge}</p>
 			</div>
 			<div className="w-full h-1/12 flex items-center justify-center">
 				<UserProfileModal />
--- src/components/home/addingSection.tsx
+++ src/components/home/addingSection.tsx
@@ -4,8 +4,9 @@ import Link from 'next/link';
 import AddingItem from './addingItem';
 import H1 from '../H1';
 import ForwardButton from '../buttons/forwardButton';
+import axiosConfigInstance from '@/config/axiosCofig';
+import { useEffect, useState } from 'react';
 import addingItemImg from '../../../public/WEBP/addingItemImg.webp';
-import emptyAddingImg from '../../../public/PNG/profile_bg.png';
 
 type AddingSectionProps = {
 	showActionButton?: boolean;
@@ -13,56 +14,28 @@ type AddingSectionProps = {
 export default function AddingSection({
 	showActionButton = true,
 }: AddingSectionProps) {
-	const addingSectionList = [
-		{
-			name: 'José De Oliveira',
-			career: 'Lic en Comunicación Social',
-			master: 'Maestrando en Innovación Educativa',
-			specialist: 'Project Manager',
-			href: '/',
-			img: addingItemImg,
-		},
-		{
-			name: 'Pedro Pérez',
-			career: 'Lic en Comunicación Social',
-			master: 'Maestrando en Innovación Educativa',
-			specialist: 'Project Manager',
-			href: '/',
-			img: emptyAddingImg,
-		},
-		{
-			name: 'María Martínez',
-			career: 'Lic en Comunicación Social',
-			master: 'Maestrando en Innovación Educativa',
-			specialist: 'Project Manager',
-			href: '/',
-			img: emptyAddingImg,
-		},
-		{
-			name: 'Luis Lagos',
-			career: 'Lic en Comunicación Social',
-			master: 'Maestrando en Innovación Educativa',
-			specialist: 'Project Manager',
-			href: '/',
-			img: emptyAddingImg,
-		},
-		{
-			name: 'Alejandra Alvarado',
-			career: 'Lic en Comunicación Social',
-			master: 'Maestrando en Innovación Educativa',
-			specialist: 'Project Manager',
-			href: '/',
-			img: emptyAddingImg,
-		},
-		{
-			name: 'Carlos Costa',
-			career: 'Lic en Comunicación Social',
-			master: 'Maestrando en Innovación Educativa',
-			specialist: 'Project Manager',
-			href: '/',
-			img: emptyAddingImg,
-		},
-	];
+	const [addingSectionList, setAddingSectionList] = useState([]);
+
+	useEffect(() => {
+		axiosConfigInstance
+			.get('/teachers')
+			.then((response) => {
+				console.log(response);
+
+				const data = response.data.map((teacher) => ({
+					name: teacher.user.first_name,
+					career: teacher.career,
+					master: teacher.master,
+					specialist: teacher.specialist,
+					href: '/',
+					img: teacher.img ?? addingItemImg,
+				}));
+				setAddingSectionList(data);
+			})
+			.catch((error) => {
+				console.error('There was an error!', error);
+			});
+	}, []);
 
 	return (
 		<Card>
@@ -70,17 +43,16 @@ export default function AddingSection({
 				<H1 className="ml-20">Sumemos</H1>
 				<div className="w-full h-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 place-items-center">
 					{addingSectionList.map((item, idx) => (
-						<AddingItem
-							{...item}
-							key={idx}
-						/>
+						<AddingItem {...item} key={idx} />
 					))}
 				</div>
+
 				{showActionButton && (
 					<div className="w-full flex justify-end my-2">
 						<Link
 							href="/connect/"
-							className="w-96 flex items-center justify-center">
+							className="w-96 flex items-center justify-center"
+						>
 							<ForwardButton text="Ver más" />
 						</Link>
 					</div>
