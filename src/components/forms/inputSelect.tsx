43e9c664407e2f6987fefad42f060ac39187919a
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-04-08 20:00:56 -0400

    chore: fix warning with defaultValue in select field

--- src/components/forms/inputSelect.tsx
+++ src/components/forms/inputSelect.tsx
@@ -26,15 +26,17 @@ export default function SelectField({
 		<select
 			className="text-sm md:text-base hover:cursor-pointer hover:bg-light-gray transform transition duration-300 w-full h-auto bg-slate-200 flex flex-row items-center justify-between px-4 py-6 rounded-2xl appearance-none"
 			{...register(label, validation)}
-			onChange={onChange}>
-			{placeholder && (
-				<option
-					value=""
-					disabled
-					selected>
-					{placeholder}
-				</option>
-			)}
+			onChange={onChange}
+			defaultValue={
+				placeholder && (
+					<option
+						value=""
+						disabled
+						selected>
+						{placeholder}
+					</option>
+				)
+			}>
 			{values.map((item, idx) => (
 				<option
 					value={item.value}
