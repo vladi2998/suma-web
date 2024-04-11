70371897ecd37cc1877cc3c988e912ad177edc7d
Daniel Vieira <danielviei@hotmail.com>
2024-04-11 19:07:09 -0400

    feat: update register to add new fields

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -42,6 +42,12 @@ export default function RegisterPage() {
 		},
 	});
 
+	const genderChoices: any[] = [
+		{ value: 'Masculino', label: 'Masculino' },
+		{ value: 'Femenino', label: 'Femenino' },
+		{ value: 'Prefiero no decirlo', label: 'Prefiero no decirlo' },
+	];
+
 	const userType: ItemValue[] = [
 		{ value: 0, label: 'Pregrado' },
 		{ value: 1, label: 'Postgrado' },
@@ -161,7 +167,8 @@ export default function RegisterPage() {
 					position: 'fixed',
 					width: '100vw',
 					height: '100vh',
-				}}>
+				}}
+			>
 				<Image
 					src={bgImageLogin}
 					alt="image-bg"
@@ -179,7 +186,8 @@ export default function RegisterPage() {
 			</div>
 			<form
 				className="w-full h-auto flex items-center justify-center z-10 overflow-y-hidden"
-				onSubmit={(e) => e.preventDefault()}>
+				onSubmit={(e) => e.preventDefault()}
+			>
 				<div className="bg-white w-4/5 sm:w-1/2 md:w-2/3 xl:w-1/3 h-auto flex flex-col items-center justify-around py-12 px-6 md:px-12 2xl:px-18 rounded-8xl space-y-12">
 					<Image
 						src={sumateLogo}
@@ -226,6 +234,18 @@ export default function RegisterPage() {
 								value === getValues('user.password') ||
 								'Las contraseñas deben ser iguales',
 						}}
+					/>
+					<SelectField
+						register={register}
+						label="user.gender"
+						errors={errors}
+						values={genderChoices}
+						placeholder="Sexo"
+					/>
+					<InputField
+						register={register}
+						label="user.exp_area"
+						placeholder="Área de experiencia"
 					/>
 					<SelectField
 						register={register}
