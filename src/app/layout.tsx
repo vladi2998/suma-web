452a618ccfca11a805a44fbe82219bb669a6708f
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-06-16 14:23:19 -0400

    feat: added smooth scroll behavior for entire page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
+		<html
+			className="scroll-smooth"
+			lang="en">
 			<body className={inter.className}>
 				<AuthProvider>
 					<UserProvider>{children}</UserProvider>
